2b20d16cfbe46dae3d584dda2e497b7aa4f4143e
Guillermo Gutierrez <guillegr123@gmail.com>
2020-10-27 13:50:20 -0600

    Get and display list of Qubicle queues

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import { Container, Header } from 'semantic-ui-react';
+import React, { useEffect, useState } from 'react';
+import { Container, Header, TableBody } from 'semantic-ui-react';
+import { Table } from 'semantic-ui-react'
 
-const Dashboard = () => (
-  <Container text>
-    <Header as='h1' dividing>
-      Queue events
-    </Header>
-  </Container>
-);
+import { listQubicleQueues } from '../utils/miniSdk';
+
+const Dashboard = () => {
+  const [queues, setQueues] = useState([]);
+  
+  useEffect(() => {
+    (async () => {
+      const queues = await listQubicleQueues();
+      setQueues(queues);
+    })();
+  }, []);
+
+  return (
+    <Container text>
+      <Header as='h1' dividing>
+        Queues
+      </Header>
+      {
+        queues.length > 0 && (
+          <Table>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>
+                  Name
+                </Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <TableBody>
+              {queues.map(queue => (
+                <Table.Row key={queue.id}>
+                  <Table.Cell>
+                    {queue.name}
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </TableBody>
+          </Table>
+        )
+      }
+    </Container>
+  );
+};
 
 export default Dashboard;
--- src/utils/miniSdk.js
+++ src/utils/miniSdk.js
@@ -1,29 +1,82 @@
 import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { RestLink } from 'apollo-link-rest';
 import gql from 'graphql-tag';
+import _ from 'lodash';
 import md5 from 'md5';
 
 import { apiUri } from '../config.json';
 
-let authToken = null;
+const localStorageSetValue = (key, value) => {
+  if (_.isString(value)) {
+    localStorage.setItem(key, value);  
+  } else {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+};
+
+const localStorageGetValue = (key, defaultValue) => {
+  const value = localStorage.getItem(key);
+
+  return _.isUndefined(value) ? defaultValue : JSON.parse(value);
+}
+
+const authRestLink = new ApolloLink((operation, forward) => {
+  if (operation.operationName === 'auth') {
+    return forward(operation).map(result => {
+      const { restResponses } = operation.getContext();
+      const authResponse = restResponses.find(resp =>
+        resp.url.endsWith('desktop_auth') && resp.status === 201
+      );
+
+      if (!authResponse) {
+        return result;
+      }
+
+      authResponse.clone().json().then(jsonResponse =>
+        localStorageSetValue('auth', {
+          authToken: jsonResponse.auth_token,
+          accountId: jsonResponse.data.account_id
+        })
+      );
+  
+      return result;
+    });
+  } else {
+    operation.setContext(({ headers }) => {
+      const authToken = localStorageGetValue('auth')?.authToken;
+      return {
+        headers: {
+          ...headers,
+          Accept: "application/json",
+          'X-Auth-Token': authToken
+        }
+      };
+    });
+
+    return forward(operation);
+  }
+});
 
 const restLink = new RestLink({
   uri: apiUri,
   responseTransformer: async response =>
-    response.json().then(jsonResponse => ({
-        ...jsonResponse.data,
-        authToken: jsonResponse.auth_token
-    })),
+    response.clone().json().then(jsonResponse => {
+      return (jsonResponse.auth_token && _.isPlainObject(jsonResponse.data))
+        ? { ...jsonResponse.data, authToken: jsonResponse.auth_token }
+        : jsonResponse.data
+    }),
   defaultSerializer: (data, headers) => {
     const serializedData = JSON.stringify({ data });
     headers.set('Content-Type', 'application/json');
     return {body: serializedData, headers};
-  }
+  },
+  fieldNameNormalizer: _.camelCase
 });
 
 const client = new ApolloClient({
-  link: restLink,
+  link: ApolloLink.from([authRestLink, restLink]),
   cache: new InMemoryCache(),
 });
 
@@ -31,7 +84,18 @@ const AUTH = gql`
   mutation auth($credentials: Credentials) {
     auth(input: $credentials)
       @rest(type: "Authentication", path: "/desktop_auth", method: "PUT") {
-        authToken
+        authToken,
+        accountId
+      }
+  }
+`;
+
+const QUBICLE_QUEUES = gql`
+  query qubicle_queues($accountId: String) {
+    qubicle_queues(accountId: $accountId)
+      @rest(type: "[QubicleQueue]", path: "/accounts/{args.accountId}/qubicle_queues", method: "GET") {
+        id,
+        name
       }
   }
 `;
@@ -44,9 +108,21 @@ export const authenticate = async ({ username, password, accountName }) => {
 
   const { data: { auth } /*, errors */ } = await client.mutate({ mutation: AUTH, variables: { credentials } });
 
-  authToken = auth.authToken;
+  localStorageSetValue('auth', auth);
 
   return auth;
 };
 
-export const isAuthenticated = () => !!authToken;
+export const isAuthenticated = () => !!localStorageGetValue('auth')?.authToken;
+
+export const listQubicleQueues = (args) => {
+  const accountId = _.get(
+    args,
+    'accountId',
+    localStorageGetValue('auth')?.accountId
+  );
+
+  return client
+    .query({ query: QUBICLE_QUEUES, variables: { accountId } })
+    .then(({ data }) => data.qubicle_queues);
+};
